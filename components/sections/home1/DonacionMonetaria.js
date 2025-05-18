95f8fee564691ee93d5961126d097e6b8b605765
Lucas <lucas@dex360.net>
2025-05-18 00:00:38 -0300

    Avances con links reales

--- components/sections/home1/DonacionMonetaria.js
+++ components/sections/home1/DonacionMonetaria.js
@@ -4,54 +4,63 @@ import { useState } from "react"
 
 export default function DonacionaMonetaria() {
     const [activeIndex, setActiveIndex] = useState(1)
-    const handleOnClick = (index) => {
+    const handleOnClick = (index, url) => {
         setActiveIndex(index)
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
     }
     return (
         <>
-        {/*Donation One Start */}
-        <section className="donation-one" id="donaciones">
-            <div className="donation-one__wrapper">
-                <div className="donation-one__left">
-                    <div >
-                        Transferencia Bancaria<br/>
-Doná el importe que quieras directamente en nuestra cuenta.
-<br/><br/>
-Banco Provincia
-<br/><br/>
-Alias: PROYECTO-4-PATAS
-<br/><br/>
-CBU: 0140059501500205063480<br/>
-Asociación Civil Proyecto 4 Patas<br/>
-Cuenta Corriente en pesos Nº 50634/8<br/>
-Sucursal Nº: 05002 – San Antonio de Padua<br/>
-CUIT: 30-71080554-3
-                    </div>
-                </div>
-                <div className="donation-one__right">
-                    <div className="donation-one__content-box">
-                        <div className="section-title-two text-left sec-title-animation animation-style2">
-                            <div className="section-title-two__tagline-box">
-                                <span className="section-title-two__tagline">Doná con Mercado Pago</span>
+            {/*Donation One Start */}
+            <section className="donation-one" id="donaciones">
+                <div className="donation-one__wrapper">
+                    <div className="donation-one__left">
+                        <div className="donation-one__content-box">
+                            <div className="section-title-two text-left sec-title-animation animation-style2">
+                                <div className="section-title-two__tagline-box">
+                                    <span className="section-title-two__tagline">Doná vía </span>
+                                </div>
+                                <h2 className="section-title-two__title title-animation">Transferencia Bancaria</h2>
                             </div>
-                            <h2 className="section-title-two__title title-animation">Tu ayuda es importante <br/> para ellos</h2>
-                        </div>
-                        <div className="donation-one__donate-box">
                             <div className="donate-amount wow fadeInUp" data-wow-delay=".5s">
-                                <button className={activeIndex == 1 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(1)}>$500</button>
-                                <button className={activeIndex == 2 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(2)}>$1000</button>
-                                <button className={activeIndex == 3 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(3)}>$2000</button>
-                                <button className={activeIndex == 4 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(4)}>$5000</button>
-                                <button className={activeIndex == 5 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(5)}>Elegí el monto</button>
-                                <button className={activeIndex == 6 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(6)}>Dona mensualmente</button>
-                                <button className={activeIndex == 7 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(7)}>Dona castración</button>
+                               <p>
+                                    <strong>Banco Provincia</strong><br />
+                                    <strong>Alias:</strong> PROYECTO-4-PATAS
+                                    <strong>CBU:</strong> 0140059501500205063480 <br />
+                                    <strong>Asociación Civil Guardianes de patitas</strong> <br />
+                                    <strong>Cuenta Corriente en pesos</strong> Nº 50634/8<br />
+                                    <strong>CUIT:</strong> 30-71080554-3
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="donation-one__right">
+                        <div className="donation-one__content-box">
+                            <div className="section-title-two text-left sec-title-animation animation-style2">
+                                <div className="section-title-two__tagline-box">
+                                    <span className="section-title-two__tagline">Doná vía </span>
+                                </div>
+                                <h2 className="section-title-two__title title-animation">Mercado Pago</h2>
+                            </div>
+                            <div className="donation-one__donate-box">
+                                <div className="donate-amount wow fadeInUp" data-wow-delay=".5s">
+                              <p>
+                                    <button className={activeIndex == 1 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(1, 'https://mpago.la/2cyGLuR')}>$500</button>
+                                    <button className={activeIndex == 2 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(2, 'https://mpago.la/2yVr2i8')}>$1000</button>
+                                    <button className={activeIndex == 3 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(3, 'https://mpago.la/1ve9GJp')}>$2000</button>
+                                    <button className={activeIndex == 4 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(4, 'https://mpago.la/2EhY261')}>$5000</button>
+                         <br /> 
+                                    <button className={activeIndex == 5 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(5, 'https://mpago.la/proyecto4patas')}>Elegí el monto</button>
+                                    <button className={activeIndex == 6 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(6, 'https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=820e1c345f0e4fb9aeee09d1287b5251')}>Dona mensualmente</button>
+                                    <button className={activeIndex == 7 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(7, 'https://mpago.la/15bcFVu')}>Dona castración</button>
+                               </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-        {/*Donation One End */}
+            </section >
+            {/*Donation One End */}
         </>
     )
 }
