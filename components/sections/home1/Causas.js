e191f4a4b22bda1e2be5ae8b72515298ef634251
Lucas <lucas@dex360.net>
2025-05-17 21:15:21 -0300

    modificando vistas

--- components/sections/home1/Causas.js
+++ components/sections/home1/Causas.js
@@ -11,8 +11,10 @@ export default function Causas() {
             </div>
             <div className="container">
                 <div className="section-title text-center sec-title-animation animation-style1">
-                  
-                    <h2 className="section-title__title title-animation">Vos podes <br/> <span>Apadrinar</span> una mascota
+                    <div className="section-title__tagline-box">
+                        <span className="section-title__tagline">Charity Fund</span>
+                    </div>
+                    <h2 className="section-title__title title-animation">Together We Can <br/> <span>Make</span> a Difference
                     </h2>
                 </div>
                 <div className="row">
@@ -21,14 +23,24 @@ export default function Causas() {
                         <div className="found-one__single">
                             <div className="found-one__img-box">
                                 <div className="found-one__img">
-                                    <img src="assets/images/apadrinar/img_heridas-gatos-curar-art.jpg" alt=""/>
-                                    <img src="assets/images/apadrinar/64f057a5fe4b3cbdb01f0a61_648b61a7a0caa6c18034f980_chatabces.webp" alt=""/>
+                                    <img src="assets/images/resources/found-1-1.jpg" alt=""/>
+                                    <img src="assets/images/resources/found-1-1.jpg" alt=""/>
                                 </div>
                             </div>
                             <div className="found-one__content">
-                                <h4 className="found-one__title"><Link href="donation-details">Rambo</Link></h4>
-                                <p className="found-one__text">Es un Gato callejero que necesita tu ayuda para su tratamiento </p>
-              
+                                <h4 className="found-one__title"><Link href="donation-details">Help For Ecosystem</Link></h4>
+                                <p className="found-one__text">Charity and Donation is a categorys that involves giving
+                                    financial </p>
+                                <div className="found-one__goals">
+                                    <div className="found-one__raised">
+                                        <p>Raised</p>
+                                        <span>40,802$</span>
+                                    </div>
+                                    <div className="found-one__raised">
+                                        <p>Goal</p>
+                                        <span>100,000$</span>
+                                    </div>
+                                </div>
                                 <div className="found-one__progress">
                                     <div className="bar">
                                         <div className="bar-inner count-bar" data-percent="75%" style={{ width: '75%' }}>
@@ -36,7 +48,7 @@ export default function Causas() {
                                     </div>
                                 </div>
                                 <div className="found-one__btn-box">
-                                    <Link href="donation-details" className="found-one__btn thm-btn">¡¡Apadrinalo ahora!!<span><i
+                                    <Link href="donation-details" className="found-one__btn thm-btn">Donate Now<span><i
                                                 className="icon-arrow-right"></i></span></Link>
                                 </div>
                             </div>
@@ -48,14 +60,24 @@ export default function Causas() {
                         <div className="found-one__single">
                             <div className="found-one__img-box">
                                 <div className="found-one__img">
-                                    <img src="assets/images/apadrinar/images.jpeg" alt=""/>
-                                    <img src="assets/images/apadrinar/1972630.webp" alt=""/>
+                                    <img src="assets/images/resources/found-1-2.jpg" alt=""/>
+                                    <img src="assets/images/resources/found-1-2.jpg" alt=""/>
                                 </div>
                             </div>
                             <div className="found-one__content">
-                                <h4 className="found-one__title"><Link href="donation-details">Hetelviro</Link></h4>
-                                <p className="found-one__text">Lo encontramos en la calle desnutrinutrido, necesita una operación </p>
-
+                                <h4 className="found-one__title"><Link href="donation-details">Water For All</Link></h4>
+                                <p className="found-one__text">Charity and Donation is a categorys that involves giving
+                                    financial </p>
+                                <div className="found-one__goals">
+                                    <div className="found-one__raised">
+                                        <p>Raised</p>
+                                        <span>40,802$</span>
+                                    </div>
+                                    <div className="found-one__raised">
+                                        <p>Goal</p>
+                                        <span>100,000$</span>
+                                    </div>
+                                </div>
                                 <div className="found-one__progress">
                                     <div className="bar">
                                         <div className="bar-inner count-bar" data-percent="75%" style={{ width: '75%' }}>
@@ -63,7 +85,7 @@ export default function Causas() {
                                     </div>
                                 </div>
                                 <div className="found-one__btn-box">
-                                    <Link href="donation-details" className="found-one__btn thm-btn">¡¡Apadrinalo ahora!!<span><i
+                                    <Link href="donation-details" className="found-one__btn thm-btn">Donate Now<span><i
                                                 className="icon-arrow-right"></i></span></Link>
                                 </div>
                             </div>
@@ -75,16 +97,32 @@ export default function Causas() {
                         <div className="found-one__single">
                             <div className="found-one__img-box">
                                 <div className="found-one__img">
-                                    <img src="assets/images/apadrinar/adoptar-a-un-perro-es-un-verdadero-acto-de-amor-y-en-buenos-aires-hay-muchos-lugares-para-encontrarse-con-una-mascota-foto-pexels-helenawlt-WVPVLPMC7KBGMC3PLL55V42CAI.jpg" alt=""/>
-                                    <img src="assets/images/apadrinar/por-que-adoptar-perros-es-mejor-que-comprar.webp" alt=""/>
+                                    <img src="assets/images/resources/found-1-3.jpg" alt=""/>
+                                    <img src="assets/images/resources/found-1-3.jpg" alt=""/>
                                 </div>
                             </div>
                             <div className="found-one__content">
-                                <h4 className="found-one__title"><Link href="donation-details">Champpion</Link></h4>
-                                <p className="found-one__text">Champion es muy alegre y quiere precisa que lo cuides y vengas a visitar</p>
-                  
+                                <h4 className="found-one__title"><Link href="donation-details">Vaccine Aid Camp</Link></h4>
+                                <p className="found-one__text">Charity and Donation is a categorys that involves giving
+                                    financial </p>
+                                <div className="found-one__goals">
+                                    <div className="found-one__raised">
+                                        <p>Raised</p>
+                                        <span>40,802$</span>
+                                    </div>
+                                    <div className="found-one__raised">
+                                        <p>Goal</p>
+                                        <span>100,000$</span>
+                                    </div>
+                                </div>
+                                <div className="found-one__progress">
+                                    <div className="bar">
+                                        <div className="bar-inner count-bar" data-percent="75%" style={{ width: '75%' }}>
+                                        </div>
+                                    </div>
+                                </div>
                                 <div className="found-one__btn-box">
-                                    <Link href="donation-details" className="found-one__btn thm-btn">¡¡Apadrinalos ahora!!<span><i
+                                    <Link href="donation-details" className="found-one__btn thm-btn">Donate Now<span><i
                                                 className="icon-arrow-right"></i></span></Link>
                                 </div>
                             </div>
--- components/sections/home1/DonacionesMonetarias.js
+++ components/sections/home1/DonacionesMonetarias.js
@@ -1,51 +1,51 @@
+'use client'
 import Link from "next/link"
+import { useState } from "react"
+
 export default function DonacionesMonetarias() {
+    const [activeIndex, setActiveIndex] = useState(1)
+    const handleOnClick = (index) => {
+        setActiveIndex(index)
+    }
     return (
         <>
-       
-       {/*Donate One Start */}
-       <section className="donate-one pdt">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-6 col-lg-6 wow slideInLeft" data-wow-delay="100ms" data-wow-duration="2500ms">
-                        <div className="donate-one__single">
-                            <div className="donate-one__single-bg"
-                                style={{ backgroundImage: ' url(assets/images/backgrounds/donate-one-single-bg.jpg)' }} >
+        {/*Donation One Start */}
+        <section className="donation-one">
+            <div className="donation-one__wrapper">
+                <div className="donation-one__left">
+                    <div className="donation-one__left-bg"
+                        style={{ backgroundImage: ' url(assets/images/backgrounds/donation-one-left-bg.jpg)' }} ></div>
+                </div>
+                <div className="donation-one__right">
+                    <div className="donation-one__content-box">
+                        <div className="section-title-two text-left sec-title-animation animation-style2">
+                            <div className="section-title-two__tagline-box">
+                                <span className="section-title-two__tagline">Join Us Now</span>
                             </div>
-                            <h3 className="donate-one__title"><Link href="donation-details">Help Them With Donation</Link>
-                            </h3>
-                            <p className="donate-one__text">Dicta sunt explicabo. Nemo enim ipsam voluptatem<br/> quia
-                                voluptas
-                                sit aspernaturaut odit aut fugit, sed<br/> quia consequuntur. Dicta sunt explicabo. Nemo
-                            </p>
-                            <div className="donate-one__btn-box">
-                                <Link href="donation-details" className="donate-one__btn thm-btn">Donate Now<span><i
-                                            className="icon-arrow-right"></i></span></Link>
-                            </div>
+                            <h2 className="section-title-two__title title-animation">Helping To Make Our <br/> World
+                                <span>Better</span> Living </h2>
                         </div>
-                    </div>
-                    <div className="col-xl-6 col-lg-6 wow slideInRight" data-wow-delay="100ms" data-wow-duration="2500ms">
-                        <div className="donate-one__single donate-one__single-2">
-                            <div className="donate-one__single-bg"
-                                style={{ backgroundImage: ' url(assets/images/backgrounds/donate-one-single-bg-two.jpg)' }} >
+                        <div className="donation-one__donate-box">
+                            <div className="donate-amount wow fadeInUp" data-wow-delay=".5s">
+                                <button className={activeIndex == 1 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(1)}>$10</button>
+                                <button className={activeIndex == 2 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(2)}>$30</button>
+                                <button className={activeIndex == 3 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(3)}>$50</button>
+                                <button className={activeIndex == 4 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(4)}>$100</button>
+                                <button className={activeIndex == 5 ? "active amount-btn" : "amount-btn"} onClick={() => handleOnClick(5)}>$200</button>
                             </div>
-                            <h3 className="donate-one__title"><Link href="become-volunteer">Join With Us To Serve As
-                                    Volenteer</Link></h3>
-                            <p className="donate-one__text">Dicta sunt explicabo. Nemo enim ipsam voluptatem<br/> quia
-                                voluptas
-                                sit aspernaturaut odit aut fugit, sed<br/> quia consequuntur. Dicta sunt explicabo. Nemo
-                            </p>
-                            <div className="donate-one__btn-box">
-                                <Link href="become-volunteer" className="donate-one__btn thm-btn">Join Now<span><i
-                                            className="icon-arrow-right"></i></span></Link>
+                            <div className="donate-now wow fadeInUp" data-wow-delay=".7s">
+                                <input type="text" className="addAmount-value" placeholder="$ Costume Amount" />
+                                <div className="donation-one__btn-box">
+                                    <Link href="#" className="donation-one__btn thm-btn">Donate Now<span><i
+                                                className="icon-arrow-right"></i></span></Link>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        {/*Donate One End */}
-           
+        {/*Donation One End */}
         </>
     )
 }
