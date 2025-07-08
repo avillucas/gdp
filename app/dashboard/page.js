13ba352fd178a8eaf11b4dcfc47420368f40776e
Lucas <lucas@dex360.net>
2025-07-07 22:10:50 -0300

    dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,16 +1,14 @@
 'use client'
 import Layout from "@/components/layout/Layout"
-import Link from "next/link";
 export default function Dashboard() {
-console.log("Dashboard page loaded");
     return (
         <>
             <Layout headerStyle={1} footerStyle={2} >
-            {/*Dashboard Page Start*/}
-            <section className="dashboard">
-               
-            </section>
-            {/*Dashboard Page End*/}
+                {/*Dashboard Page Start*/}
+                <section className="dashboard">
+                    <h1> Bienvenido al administrador!!</h1>
+                </section>
+                {/*Dashboard Page End*/}
             </Layout>
         </>
     )
